8b2bd7fe3375eeb2e3d8246ea516d30b2036011c
nadav <nadaveli88@gmail.com>
2019-04-10 23:05:09 +0300

    change position of the loader gif when searching for products

---
+++ SImpleWebApp/Models/ProductsDataObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SImpleWebApp.Models
+{
+
+    public class ProductsDataObject
+    {
+        public string searchValue { get; set; }
+        public List<SImpleWebApp.Models.T_products> productList { get; set; }
+
+    }
+}
